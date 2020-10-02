85d371eae0b5c1830366a7500b164926951fd479
Zsuzsanna Urban <urbanzsuzsii@gmail.com>
2020-10-02 15:34:06 +0200

    constant helper values collected in a separate js file

---
+++ js-numerals/numbers-to-words/src/services/ConstantValues.js
@@ -0,0 +1,44 @@
+export const ONES_TO_TWENTY = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "eleven",
+  "twelve",
+  "thirteen",
+  "fourteen",
+  "fifteen",
+  "sixteen",
+  "seventeen",
+  "eighteen",
+  "nineteen",
+];
+
+export const TENS_TO_HUNDRED = [
+  "zero",
+  "ten",
+  "twenty",
+  "thirty",
+  "forty",
+  "fifty",
+  "sixty",
+  "seventy",
+  "eighty",
+  "ninety",
+];
+
+export const TEN = 10;
+export const ONE_HUNDRED = 100;
+export const ONE_THOUSAND = 1000;
+export const ONE_MILLION = 1000000; //1 000 000
+export const ONE_BILLION = 1000000000; //1 000 000 000
+export const ONE_TRILLION = 1000000000000; //1 000 000 000 000
+export const ONE_QUADRILLION = 1000000000000000; //1 000 000 000 000 000
+export const MAX_SAFE_NUMBER = Number.MAX_SAFE_INTEGER; //9 007 199 254 740 991
--- js-numerals/numbers-to-words/src/services/Converter.js
+++ js-numerals/numbers-to-words/src/services/Converter.js
@@ -1,6 +1,10 @@
+import * as Constants from "./ConstantValues.js";
+
 const convertToWords = (number) => {
   //TODO
   return number;
 };
+
+const createWordFromNumber = (number) => {};
 
 export default convertToWords;
