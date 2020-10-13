1ecddc403f69cb275f7e6ac98dc1f9ee1bdd3807
Zsuzsanna Urban <urbanzsuzsii@gmail.com>
2020-10-13 09:21:33 +0200

    header modified in api calls

--- js-users/users-app/src/utils/apiCalls.js
+++ js-users/users-app/src/utils/apiCalls.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const baseURL = "http://js-assessment-backend.herokuapp.com/users.json";
+const baseURL = "http://js-assessment-backend.herokuapp.com/users";
+let myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
 export const getAllUsers = () => {
-  return axios.get(baseURL).then((res) => res.data);
+  return axios
+    .get(baseURL, {
+      headers: myHeaders,
+    })
+    .then((res) => res.data);
 };
 
-export const addNewUser = (userData) => {
-  return axios.post(baseURL, userData).then((res) => res.data);
-};
+export const addNewUser = (userData) => {};
