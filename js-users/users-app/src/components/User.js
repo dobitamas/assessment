5237fe9ea3661bd3723af6d264932ff6a297a47f
Zsuzsanna Urban <urbanzsuzsii@gmail.com>
2020-10-13 12:06:50 +0200

    status toggle button added, attributes strikethroughed when locked

--- js-users/users-app/src/components/User.js
+++ js-users/users-app/src/components/User.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { toggleStatus } from "../utils/apiCalls";
 
 export default function User(properties) {
+  const [status, setStatus] = useState(properties.user.status);
+
+  const updateStatus = (event) => {
+    event.preventDefault();
+    let newStatus = status === "locked" ? "active" : "locked";
+    toggleStatus(newStatus, properties.user.id);
+    setStatus(newStatus);
+  };
+
+  const attributeStyle = {
+    textDecoration: status === "locked" ? "line-through" : "none",
+  };
+
   return (
     <div>
-      <h1>{properties.user.first_name}</h1>
-      <h1>{properties.user.last_name}</h1>
-      <h1>{properties.user.created_at}</h1>
+      <h1 style={attributeStyle}>{properties.user.first_name}</h1>
+      <h1 style={attributeStyle}>{properties.user.last_name}</h1>
+      <h1 style={attributeStyle}>{properties.user.created_at}</h1>
+      <button onClick={updateStatus}>toggle status</button>
     </div>
   );
 }
--- js-users/users-app/src/utils/apiCalls.js
+++ js-users/users-app/src/utils/apiCalls.js
@@ -1,15 +1,22 @@
 import axios from "axios";
 
 const baseURL = "http://js-assessment-backend.herokuapp.com/users";
-let myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+const headers = { "Content-Type": "application/json" };
 
 export const getAllUsers = () => {
+  const getHeaders = new Headers(headers);
   return axios
     .get(baseURL, {
-      headers: myHeaders,
+      headers: getHeaders,
     })
-    .then((res) => res.data);
+    .then((response) => response.data);
 };
 
-export const addNewUser = (userData) => {};
+export const toggleStatus = (updatedStatus, userId) => {
+  const requestData = { status: updatedStatus };
+  return axios.put(`${baseURL}/${userId}.json`, requestData, {
+    headers: headers,
+  });
+};
+
+export const addNewUser = (firstName, lastName) => {};
